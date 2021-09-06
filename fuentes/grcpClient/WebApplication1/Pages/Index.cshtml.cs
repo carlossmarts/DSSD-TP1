f48458855bb82aa8df342e512e62ca4eb70eb041
Nicolas <fiaschenicolas@gmail.com>
2021-09-06 17:59:24 -0300

    new client

---
+++ fuentes/grcpClient/WebApplication1/Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public ListMedicamentoDTO Message { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task OnGetAsync()
+        {
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            using var channel = GrpcChannel.ForAddress("http://localhost:9000");
+            var med = new Medicamento.MedicamentoClient(channel);
+
+            var reply = await med.getAllMedicamentoAsync(new Empty { });
+
+            this.Message = reply;
+        }
+    }
+}
